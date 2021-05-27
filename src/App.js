833c70aeeb609ec375bb92ca23e11c164c4b5781
Vimal Munjani <vimal.j.munjani@gmail.com>
2021-05-27 16:08:28 +0530

    Add: webpack config

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>Hello!! This is React Scratchpad</div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
+}
